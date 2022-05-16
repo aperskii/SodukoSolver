15383a6087dca30ace10801cd3d4afd6c90496b0
Yassine Berqiqch <101721381+aperskii@users.noreply.github.com>
2022-05-16 05:07:42 +0100

    Initial commit

---
+++ SodukoSolver/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace SodukoSolver
+{
+    partial class Soduko
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._solve = new System.Windows.Forms.Button();
+            this._new = new System.Windows.Forms.Button();
+            this.sudukoGrid = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _solve
+            // 
+            this._solve.Location = new System.Drawing.Point(669, 175);
+            this._solve.Name = "_solve";
+            this._solve.Size = new System.Drawing.Size(75, 23);
+            this._solve.TabIndex = 1;
+            this._solve.Text = "Solve";
+            this._solve.UseVisualStyleBackColor = true;
+            this._solve.Click += new System.EventHandler(this._solve_Click);
+            // 
+            // _new
+            // 
+            this._new.Location = new System.Drawing.Point(669, 120);
+            this._new.Name = "_new";
+            this._new.Size = new System.Drawing.Size(75, 23);
+            this._new.TabIndex = 2;
+            this._new.Text = "New";
+            this._new.UseVisualStyleBackColor = true;
+            this._new.Click += new System.EventHandler(this._new_Click);
+            // 
+            // sudukoGrid
+            // 
+            this.sudukoGrid.Location = new System.Drawing.Point(589, 94);
+            this.sudukoGrid.Name = "sudukoGrid";
+            this.sudukoGrid.Size = new System.Drawing.Size(224, 20);
+            this.sudukoGrid.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(586, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Suduko grid";
+            // 
+            // Soduko
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(825, 610);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sudukoGrid);
+            this.Controls.Add(this._new);
+            this.Controls.Add(this._solve);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Soduko";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button _solve;
+        private System.Windows.Forms.Button _new;
+        private System.Windows.Forms.TextBox sudukoGrid;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SodukoSolver/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SodukoSolver
+{
+    public partial class Soduko : Form
+    {
+
+        RichTextBox[,] richBoxes = new RichTextBox[9, 9];
+        char[,] sudoku = new char[,]
+        {
+            { '5', '3', '0', '0', '7', '0', '0', '0', '0' },
+            { '6', '0', '0', '1', '9', '5', '0', '0', '0' },
+            { '0', '9', '8', '0', '0', '0', '0', '6', '0' },
+            { '8', '0', '0', '0', '6', '0', '0', '0', '3' },
+            { '4', '0', '0', '8', '0', '3', '0', '0', '1' },
+            { '7', '0', '0', '0', '2', '0', '0', '0', '6' },
+            { '0', '6', '0', '0', '0', '0', '2', '8', '0' },
+            { '0', '0', '0', '4', '1', '9', '0', '0', '5' },
+            { '0', '0', '0', '0', '8', '0', '0', '7', '9' }
+        };
+        public Soduko()
+        {
+            InitializeComponent();
+            InitializeSoduku();
+            
+        }
+
+        private void InitializeSoduku()
+        {
+            Panel panel = new Panel();
+            for (int i = 0; i < richBoxes.GetLength(0); i++)
+            {
+                for (int j = 0; j < richBoxes.GetLength(1); j++)
+                {
+                    richBoxes[i, j] = new RichTextBox();
+                    richBoxes[i, j].Size = new Size(40, 40);
+                    richBoxes[i, j].Location = new Point(BoxPadding(i) + 2, 2 + BoxPadding(j));
+                    richBoxes[i, j].ReadOnly = true;
+                    richBoxes[i, j].Parent = panel;
+                    richBoxes[i, j].Text = GetNumber(i, j);
+                    richBoxes[i, j].Font = new Font("arial", 27.0f);
+                    richBoxes[i, j].ScrollBars = RichTextBoxScrollBars.None;
+                    richBoxes[i, j].SelectionAlignment = HorizontalAlignment.Center;
+                }
+            }
+            panel.Parent = this;
+            panel.BackColor = Color.Black;
+            panel.Size = new Size(384, 384);
+            panel.Location = new Point(23, 28);
+        }
+
+        /// <summary>
+        /// calculates the padding value and returns the result
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private int BoxPadding(int i)
+        {
+            if(i >= 3 && i < 6)
+            {
+                return 42 * i;
+            }
+            if(i >= 6)
+                return 42 * i+2;
+            return 41 * i;
+        }
+
+        /// <summary>
+        /// returns the number from sudoku array with i and j indexes
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        private string GetNumber(int i, int j)
+        {
+            if (sudoku[i, j] != '0')
+            {
+                return Convert.ToString(sudoku[i, j]);
+            }
+            else return "";
+        }
+
+        public void SolveSudoku(char[,] board)
+        {
+            if (board == null || board.Length == 0)
+                return;
+            Rolve(sudoku);
+            FillBoxes(sudoku);
+        }
+
+        private void FillBoxes(char[,] board)
+        {
+            for (int i = 0; i < richBoxes.GetLength(0); i++)
+            {
+                for (int j = 0; j < richBoxes.GetLength(1); j++)
+                {
+                    richBoxes[i, j].Text = GetNumber(i, j);
+                }
+            }
+        }
+
+
+        public bool Rolve(char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == '0')
+                    {
+                        for (char c = '1'; c <= '9'; c++)
+                        {
+                            if (IsValid(board, i, j, c))
+                            {
+                                board[i, j] = c;
+                                if (Rolve(board)) { 
+                                    return true;
+                                }
+                                else
+                                    board[i, j] = '0';
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static bool IsValid(char[,] board, int row, int col, char c)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                //check row  
+                if (board[i, col] != '0' && board[i, col] == c)
+                    return false;
+                //check column  
+                if (board[row, i] != '0' && board[row, i] == c)
+                    return false;
+                //check 3*3 block  
+                if (board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3] != '0' && board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3] == c)
+                    return false;
+            }
+            return true;
+        }
+
+        private void _solve_Click(object sender, EventArgs e)
+        {
+            SolveSudoku(sudoku);
+        }
+
+        private void _new_Click(object sender, EventArgs e)
+        {
+            string grid = sudukoGrid.Text;
+            int index = 0;
+            for (int i = 0; i < sudoku.GetLength(0); i++)
+            {
+                for (int j = 0; j < sudoku.GetLength(1); j++)
+                {
+                    sudoku[i, j] = grid[index];
+                    index++;
+                }
+            }
+            FillBoxes(sudoku);
+        }
+    }
+}
